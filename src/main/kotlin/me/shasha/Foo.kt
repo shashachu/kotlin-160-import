5860add9feb5a538ae906b5a6a4da948057404e5
Sha Sha Chu <shasha@pinterest.com>
2021-12-09 10:59:21 -0800

    Sample project to demonstrate import regression in Kotlin 1.6.0

---
+++ src/main/kotlin/me/shasha/Foo.kt
@@ -0,0 +1,7 @@
+package me.shasha
+
+class Foo {
+    companion object {
+        const val MY_CONSTANT = 1
+    }
+}
---
+++ src/main/kotlin/me/shasha/Main.kt
@@ -0,0 +1,8 @@
+package me.shasha
+
+import me.shasha.Foo.Companion
+        .MY_CONSTANT
+
+fun main() {
+    val blah = MY_CONSTANT
+}
